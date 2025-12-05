c6a834f27c85f429ddbf5bd636cff54cbb63caae
VerdaMech <verdaguergvice@gmail.com>
2025-12-05 20:33:01 -0300

    Arreglando los Tests

--- app/src/androidTest/java/com/example/deckora/CameraScreenTest.kt
+++ app/src/androidTest/java/com/example/deckora/CameraScreenTest.kt
@@ -6,60 +6,86 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.rememberNavController
 import com.example.deckora.ui.screens.CameraScreen
+import com.example.deckora.viewmodel.CartaViewModel
 import com.example.deckora.viewmodel.MainViewModel
+import com.example.deckora.viewmodel.ResumenViewModel
+import com.example.deckora.viewmodel.UsuarioViewModel
 import org.junit.Rule
 import org.junit.Test
-
 
 class CameraScreenTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    private val dummyViewModel = MainViewModel()
+    private val dummyMainViewModel = MainViewModel()
 
+    private val dummyCartaViewModel = CartaViewModel()
+
+    private val dummyResumenViewModel = ResumenViewModel()
+
+    private val dummyUsuarioViewModel = UsuarioViewModel()
 
 
     @Test
     fun testVistaCameraScreen() {
 
-
         composeTestRule.setContent {
             val navController = rememberNavController()
-            CameraScreen(navController = navController,
-                viewModel = dummyViewModel
+
+            CameraScreen(
+                navController = navController,
+                viewModel = dummyMainViewModel,
+                usuarioViewModel = dummyUsuarioViewModel,
+                cartaViewModel = dummyCartaViewModel,
+                resumenViewModel = dummyResumenViewModel
             )
         }
 
-
-        composeTestRule.onNodeWithText("¡Agrega una nueva foto a tu colección!")
+        // ----------- Texto principal -----------
+        composeTestRule.onNodeWithText("Subir una foto")
             .assertIsDisplayed()
 
-
-        composeTestRule.onNodeWithText("Agregar de galería")
+        composeTestRule.onNodeWithText("¡Agrega una nueva carta!")
             .assertIsDisplayed()
 
-        composeTestRule.onNodeWithText("Tomar Foto")
+        // ----------- Botón -----------
+        composeTestRule.onNodeWithText("Galería")
             .assertIsDisplayed()
 
-
-        composeTestRule.onNodeWithText("home")
-            .assertIsDisplayed()
-
-        composeTestRule.onNodeWithText("profile")
-            .assertIsDisplayed()
-
-        composeTestRule.onNodeWithText("camera")
-            .assertIsDisplayed()
-
-        composeTestRule.onNodeWithText("carpeta")
-            .assertIsDisplayed()
-
-
-        composeTestRule.onNodeWithText("Agregar de galería")
+        composeTestRule.onNodeWithText("Galería")
             .performClick()
 
         composeTestRule.onNodeWithText("Tomar Foto")
+            .assertIsDisplayed()
+
+        composeTestRule.onNodeWithText("Tomar Foto")
             .performClick()
+
+        // ----------- Barra inferior -----------
+        composeTestRule.onNodeWithText("Cartas Top")
+            .assertIsDisplayed()
+
+        composeTestRule.onNodeWithText("Cartas Top")
+            .performClick()
+
+        composeTestRule.onNodeWithText("Tú Perfil")
+            .assertIsDisplayed()
+
+        composeTestRule.onNodeWithText("Tú Perfil")
+            .performClick()
+
+        composeTestRule.onNodeWithText("Cámara")
+            .assertIsDisplayed()
+
+        composeTestRule.onNodeWithText("Cámara")
+            .performClick()
+
+        composeTestRule.onNodeWithText("Carpetas")
+            .assertIsDisplayed()
+
+        composeTestRule.onNodeWithText("Carpetas")
+            .performClick()
+
     }
 }
